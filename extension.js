65e9c7332269bdb764b2d05df421ff6b1d4f70fe
Kamil Páral <kamil.paral@gmail.com>
2012-04-17 15:27:31 +0200

    hide the button if anaconda desktop file is hidden as well

--- extension.js
+++ extension.js
@@ -4,40 +4,43 @@ const Shell = imports.gi.Shell;
 
 let button, app;
 
+// execute anaconda after button has been clicked
 function _runAnaconda() {
     app.activate();
 }
 
+// init() runs on gnome-shell start
 function init() {
-
-    // find anaconda.desktop app
-    let shell = new Shell.AppSystem.get_default();
+    // == find anaconda.desktop app ==
+    shell = new Shell.AppSystem.get_default();
     apps = shell.get_all().filter(function(app) {
-        return app.get_id() == 'anaconda.desktop'
+        // liveinst.desktop is renamed to anaconda.desktop on LiveCD
+        return app.get_id() == 'anaconda.desktop' || app.get_id() == 'liveinst.desktop';
     });
     if (apps.length <= 0) {
-        // we will report an error
+        // anaconda is not present or its desktop file is hidden
+        // we don't want to display this extension in that case either
+        global.log('Anaconda not installed or hidden, not showing InstallFedoraButton extension');
+        return;
     } else {
-        app = apps[0];
+        app = apps[0]; //there should be only one
     };
 
-    // create UI
+    // == create UI ==
     box = new St.BoxLayout();
 
-    if (app != null) {
-    let height = Main.panel._activitiesButton.actor.get_height();
-        let icon = app.create_icon_texture(height);
-        box.add(icon);
-    };
+    // add icon
+    height = Main.panel._activitiesButton.actor.get_height(); // get ideal icon size
+    icon = app.create_icon_texture(height);
+    box.add(icon);
 
-    let text = 'Anaconda not found';
-    if (app != null) {
-        text = app.get_name();
-    };
-    let label = new St.Label({ text: text,
-                   style_class: 'text-label' });
+    // add text
+    text = app.get_name();
+    label = new St.Label({ text: text,
+                           style_class: 'text-label' });
     box.add(label);
 
+    // create a button
     button = new St.Bin({ style_class: 'panel-button',
                           reactive: true,
                           can_focus: true,
@@ -46,10 +49,16 @@ function init() {
     button.connect('button-press-event', _runAnaconda);
 }
 
+// enable this extension
 function enable() {
-    Main.panel._rightBox.insert_child_at_index(button, 0);
+    if (button != null) {
+        Main.panel._rightBox.insert_child_at_index(button, 0);
+    };
 }
 
+// disable this extension, revert all changes
 function disable() {
-    Main.panel._rightBox.remove_child(button);
+    if (button != null) {
+        Main.panel._rightBox.remove_child(button);
+    }
 }
